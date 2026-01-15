409b755a3c760471c42d3a7f6cfff1cdc131d4d5
Abdullah Zafar <abdullah.zafar@directfn.com>
2026-01-15 19:01:04 +0500

    Component created

---
+++ src/api/locationAPI.tsx
@@ -0,0 +1,36 @@
+import axios from './axios';
+
+export const shareLocationAPI = async (
+  latitude: number,
+  longitude: number,
+  visibility: string,
+  type: string = 'live',
+  duration: number = 24
+) => {
+  const res = await axios.post('/location/share', {
+    latitude,
+    longitude,
+    visibility,
+    type,
+    duration,
+  });
+  return res.data;
+};
+
+export const getSharedLocationsAPI = async (
+  lat?: number,
+  lon?: number,
+  radius?: number,
+  targetUserId?: string,
+  timePeriod?: string
+) => {
+  const res = await axios.get('/location/shared', {
+    params: { lat, lon, radius, targetUserId, timePeriod },
+  });
+  return res.data;
+};
+
+export const stopSharingAPI = async () => {
+  const res = await axios.delete('/location/stop');
+  return res.data;
+};
