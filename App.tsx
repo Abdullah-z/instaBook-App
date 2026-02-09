11738c73fb00854e0dfac6bd4b1a9693943bfe56
Abdullah Zafar <abdullah.zafar@directfn.com>
2026-02-09 17:59:04 +0500

    Support for new colors added

--- App.tsx
+++ App.tsx
@@ -20,11 +20,26 @@ import { OrangeLight } from './src/constants/themes/OrangeLight';
 import { OrangeDark } from './src/constants/themes/OrangeDark';
 import { RedDark } from './src/constants/themes/RedDark';
 import { PurpleDark } from './src/constants/themes/PurpleDark';
+import { NeonPinkLight } from './src/constants/themes/NeonPinkLight';
+import { NeonPinkDark } from './src/constants/themes/NeonPinkDark';
+import { NeonCyanLight } from './src/constants/themes/NeonCyanLight';
+import { NeonCyanDark } from './src/constants/themes/NeonCyanDark';
+import { NeonLimeLight } from './src/constants/themes/NeonLimeLight';
+import { NeonLimeDark } from './src/constants/themes/NeonLimeDark';
+import { ElectricBlueLight } from './src/constants/themes/ElectricBlueLight';
+import { ElectricBlueDark } from './src/constants/themes/ElectricBlueDark';
 import { DataProvider, useData, usePushNotifications } from './src/hooks';
 import * as Notifications from 'expo-notifications';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { useFonts } from 'expo-font';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from '@expo-google-fonts/inter';
 import * as SplashScreen from 'expo-splash-screen';
 
 // Keep the splash screen visible while we fetch resources
@@ -32,12 +47,11 @@ SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
-    'Inter-Regular':
-      'https://github.com/google/fonts/raw/main/ofl/inter/Inter%5Bslnt%2Cwght%5D.ttf',
-    'Inter-Medium': 'https://github.com/google/fonts/raw/main/ofl/inter/Inter%5Bslnt%2Cwght%5D.ttf',
-    'Inter-SemiBold':
-      'https://github.com/google/fonts/raw/main/ofl/inter/Inter%5Bslnt%2Cwght%5D.ttf',
-    'Inter-Bold': 'https://github.com/google/fonts/raw/main/ofl/inter/Inter%5Bslnt%2Cwght%5D.ttf',
+    'Inter-Regular': Inter_400Regular,
+    'Inter-Medium': Inter_500Medium,
+    'Inter-SemiBold': Inter_600SemiBold,
+    'Inter-Bold': Inter_700Bold,
+    'Inter-Black': Inter_900Black,
   });
 
   useEffect(() => {
@@ -68,15 +82,19 @@ function MainApp() {
   if (!themeLoaded) return null; // Or a splash screen
 
   const themeMap = {
-    b: isDark ? BlueDark : BlueLight,
     g: isDark ? GreenDark : GreenLight,
-    y: isDark ? YellowDark : YellowLight,
-    r: isDark ? RedDark : RedLight,
+    b: isDark ? BlueDark : BlueLight,
     p: isDark ? PurpleDark : PurpleLight,
+    r: isDark ? RedDark : RedLight,
     o: isDark ? OrangeDark : OrangeLight,
+    y: isDark ? YellowDark : YellowLight,
+    np: isDark ? NeonPinkDark : NeonPinkLight,
+    nc: isDark ? NeonCyanDark : NeonCyanLight,
+    nl: isDark ? NeonLimeDark : NeonLimeLight,
+    eb: isDark ? ElectricBlueDark : ElectricBlueLight,
   };
 
-  const theme = themeMap[themeColor as keyof typeof themeMap] || themeMap['r'];
+  const theme = themeMap[themeColor as keyof typeof themeMap] || themeMap['g'];
 
   return (
     <AuthProvider>
